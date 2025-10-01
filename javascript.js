1b4b333e2b549d76b9bcab6b4f2cca50d43b9001
bennyberg <73042035+bennyberg@users.noreply.github.com>
2025-10-01 09:32:57 +0300

    initial commit and added operate function

---
+++ javascript.js
@@ -0,0 +1,25 @@
+let left = 0;
+let right = 0;
+let operator = "";
+
+const operate = function(op, left, right){
+    switch(op){
+        case '+':
+            return left + right;
+        case '-':
+            return left - right;
+        case '*':
+            return left * right;
+        case '/':
+            if (right === 0)
+            {
+                return "Division by zero";
+            }
+            return left / right;
+        default:
+            return "Invalid operator";
+    }
+}
+
+
+// console.log(operate('*', 2 ,3));
